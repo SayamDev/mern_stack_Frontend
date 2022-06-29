d9761f440b30dd37797ef490eb3658691741c349
Sayam Ajmal <itssanesayam@gmail.com>
2022-06-29 15:45:51 +0100

    Added React Context

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,12 @@
-  import { useEffect, useState } from "react"
+import { useEffect } from "react"
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext"
 
-  //components
-  import WorkoutDetail from '../components/WorkoutDetail'
-  import WorkoutForm from "../components/WorkoutForm"
-
+// components
+import WorkoutDetail from "../components/WorkoutDetail"
+import WorkoutForm from "../components/WorkoutForm"
 
 const Home = () => {
-  const [workouts, setWorkouts] = useState(null)
+  const { workouts, dispatch } = useWorkoutsContext()
 
   useEffect(() => {
     const fetchWorkouts = async () => {
@@ -14,18 +14,18 @@ const Home = () => {
       const json = await response.json()
 
       if (response.ok) {
-        setWorkouts(json)
+        dispatch({type: 'SET_WORKOUTS', payload: json})
       }
     }
 
     fetchWorkouts()
-  }, [])
+  }, [dispatch])
 
   return (
     <div className="home">
       <div className="workouts">
         {workouts && workouts.map(workout => (
-          <WorkoutDetail  key={workout._id} workout={workout}/>
+          <WorkoutDetail workout={workout} key={workout._id} />
         ))}
       </div>
       <WorkoutForm />
